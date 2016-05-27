86bc78e479c0ed5b31f72a6b540a8beba2a1a8b5
laksith <lrbekanayake@hotmail.com>
2016-05-27 14:23:24 +0530

    hhhhgfgfgfg

--- testpy.py
+++ testpy.py
@@ -19,7 +19,7 @@ class myHandler(BaseHTTPRequestHandler):
 try:
     #Create a web server and define the handler to manage the
     #incoming request
-    server = HTTPServer(('', PORT_NUMBER), myHandler)
+    server = HTTPuServer(('', PORT_NUMBER), myHandler)
     print 'Started httpserver on port ' , PORT_NUMBER
 
     #Wait forever for incoming htto requests
